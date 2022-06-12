e60eabfb7a2cd15970e16af513aa13df37727e9e
jilliennelapid <jillienne.lapid@gmail.com>
2022-06-12 19:57:15 -0400

    Completed exercise

--- app.js
+++ app.js
@@ -4,7 +4,32 @@ console.log(
 );
 
 // Exercise 1
+//    const firstName = "";
+//    let lastName = "";
+//    var age = --;
+
 
 // Exercise 2
+const firstName = "Jillienne";
+let lastName = "Lapid";
+var age = 18;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePrecentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
